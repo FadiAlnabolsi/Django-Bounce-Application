f861d1869441b81168a044931c3c1c57171a7ce9
Alnabolsi, Fadi <Fadi@Service.com>
2015-07-13 13:42:09 -0400

    RND102.04 Commit 1, Added title and header to homepage

--- functional_tests.py
+++ functional_tests.py
@@ -13,10 +13,18 @@ class HomepageTest(unittest.TestCase):
         # Charlie opens his web browser and goes to the Bounce homepage
         self.browser.get('http://localhost:8000')
 
+        # He sees "Bounce" in the page header and browser title
+        self.assertEqual(self.browser.title, "Bounce")
+        header_text = self.browser.find_element_by_css_selector('header h1').text
+        self.assertEqual(header_text, "Bounce")
+
         # He sees fifteen interesting links to check out
         links = self.browser.find_elements_by_css_selector('li.link a')
         self.assertEqual(len(links), 15)
 
+        for link in links:
+        	self.assertTrue(link.get_attribute("href"))
+        	self.assertTrue(link.text)
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
